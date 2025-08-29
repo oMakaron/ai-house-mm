e4d0ce2f565fe72157709843082a88b26c8fafb6
oMakaron <nickalexander8257@gmail.com>
2025-08-29 18:41:42 +0800

    prompt update and stop upon finishing

--- profile_maker.py
+++ profile_maker.py
@@ -8,9 +8,12 @@ def profileAI(client: anthropic.Anthropic, history: list):
                                       messages=history)
     history.append({"role": "assistant", "content": response.content[0].text})
     print(response.content[0].text)
+    if ('{' in response.content[0].text):
+        return False
+    return True
 
 def main():
-    key = 'your key here'
+    key = 'your api key'
     client = anthropic.Anthropic(api_key=key)
     history = [{'role': 'user',
                 'content': 'You are a friendly pal trying to help me as a user to register an account for Perth house finder app. '
@@ -19,15 +22,20 @@ def main():
                 '{{"name": (my name), "dob": (my birthdate), "phone": (my number), "email": (my email), "job": (my occupation), "about": (about myself)}}\n'
                 'change all brackets with a string of answers you get from my answers to your questions. '
                 'for the about myself part, summarize it to be less than 50 words if it exceeds 50 words. '
-                'Now you can start by introducing yourself and ask the first question about my name'}]
+                'for the birthdate make it in the format of DD/MM/YYYY. '
+                'the phone number is in the string with format of numbers with no spaces in between them. '
+                'make sure when all of the information is completed and you are going to give the JSON response, dont give any comments! just give a raw JSON format. '
+                'also translate all non english response like in occupation and about myself into english if the user decided to use another language than english' 
+                'Now you can start by introducing yourself and ask the prefered language and ask my identity with the prefered language'}]
     
     response = client.messages.create(model="claude-sonnet-4-20250514",
                                       max_tokens=1000,
                                       messages=history)
     history.append({"role": "assistant", "content": response.content[0].text})
     print(response.content[0].text)
-    while(True):
-        profileAI()
+    flag = True
+    while(flag):
+        flag = profileAI(client, history)
 
 if __name__ == '__main__':
     main()
