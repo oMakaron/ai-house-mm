84aa01513ea87cedac049fe1f3d26c3d46822123
oMakaron <nickalexander8257@gmail.com>
2025-08-29 22:51:47 +0800

    add example prompt and fix prompting and naming

--- matchmaker.py
+++ matchmaker.py
@@ -2,7 +2,7 @@ import pandas as pd
 import helper
 import anthropic
 
-def main(user_info: dict):
+def MMmain(user_info: dict):
     client = helper.get_anthropic()
     housedf = pd.read_csv('housemock.csv')
     housedfstr = housedf.to_string(index=False)
@@ -20,6 +20,10 @@ def main(user_info: dict):
     """
     response = client.messages.create(model="claude-sonnet-4-20250514",
                                       max_tokens=1000,
-                                      messages=prompt)
+                                      messages=[{"role": "user", "content": prompt}])
 
     return response.content[0].text
+
+if __name__ == '__main__':
+    res = MMmain({"name": "Nick Polish", "dob": "24/11/2000", "phone": "0416305461", "email": "nick@gmail.com", "job": "student", "suburb": "Crawley", "about": "I am a sociable student looking for a house", 'prefer': 'I would like a place to live near my campus at UWA, ideally I would not need to waste more than 1 hour on travel time'})
+    print(res)
